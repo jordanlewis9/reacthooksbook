9c7c1c32a64932bcda5a399b506a17cdcf04886b
Jordan <jlewis2008@live.com>
2020-08-15 21:04:13 -0400

    Through chapter 5

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import JumbotronComponent from "./JumbotronComponent";
+import Products from "./Products";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <JumbotronComponent>
+        This is a long sentence, and I want to insert content into the jumbotron
+        component from the outside.
+      </JumbotronComponent>
     </div>
   );
 }
