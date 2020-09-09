bc271ac2e56e557e5348e09963eff5a2cae9efdc
Jordan <jlewis2008@live.com>
2020-09-09 17:26:33 -0400

    Completed simple GitHub API rendering

--- src/GitHub.js
+++ src/GitHub.js
@@ -1,14 +1,63 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import ReactLoading from "react-loading";
+import { Media } from "react-bootstrap";
+import { getDefaultNormalizer } from "@testing-library/react";
 
 function GitHub() {
+  const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("greg");
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
-    axios.get("https://api.github.com/search/users?q=greg").then((res) => {
-      console.log(res.data.items);
-    });
+    getData();
   }, []);
 
-  return <div></div>;
+  const getData = async () => {
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${searchTerm}`
+    );
+    setData(res.data.items);
+    setIsLoading(false);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    getData();
+  };
+
+  const listUsers = data.map((user) => {
+    return (
+      <Media key={user.id}>
+        <a href={user.html_url}>
+          <img
+            width={64}
+            height={64}
+            className="mr-3"
+            src={user.avatar_url}
+            alt="User Image"
+          />
+        </a>
+        <Media.Body>
+          <h5>Login: {user.login}</h5>
+          <p>Id: {user.id}</p>
+        </Media.Body>
+      </Media>
+    );
+  });
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={(e) => setSearchTerm(e.target.value)} />
+        <button type="submit">Search</button>
+      </form>
+      <h3>GitHub User Results</h3>
+      {isLoading && <ReactLoading type="bars" color="#444" />}
+      {listUsers}
+    </div>
+  );
 }
 
 export default GitHub;
